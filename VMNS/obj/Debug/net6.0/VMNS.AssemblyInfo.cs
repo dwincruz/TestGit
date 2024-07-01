7cf95a2b307f36b3aa3cc222db4a93fc04a7ce24
HoodiScript <acecruz14.e@gmail.com>
2024-07-01 15:11:41 +0800

    Add Remarks to modal, change "Remarks" text to "Description, add all remarks of parts category to the modal

--- VMNS/obj/Debug/net6.0/VMNS.AssemblyInfo.cs
+++ VMNS/obj/Debug/net6.0/VMNS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VMNS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e908299616e35b071cf5fc9e9d782551a2746606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828461e2e948ef407d9ba387392a45690120dfdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VMNS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VMNS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
